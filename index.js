19888ab8d0c4458050e674ebf44dcc41c7cf6313
Isaias Medina <isamed92@gmail.com>
2022-07-16 01:44:49 -0300

    add listener and first endpoint to index.js

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log('hola mundo!')
+
+
+const express = require('express');
+
+
+// Create an Express Server
+
+const app = express()
+
+//endpoints 
+
+app.get('/', (req, res) => {
+
+    res.json({
+        ok: true
+    })
+
+})
+
+
+// Listening peticions
+
+app.listen(4200, () => {
+
+    console.log(`Server Running on PORT: ${4200}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
